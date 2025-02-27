eca0461ab058aff7eb44b8f77207ab800760eda3
Bhoomikakaushik <151210021+Bhoomikakaushik@users.noreply.github.com>
2025-02-28 01:01:55 +0530

    Fixed Google OAuth callback URL

--- app.js
+++ app.js
@@ -102,7 +102,10 @@ passport.use( new GoogleStrategy(
     {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:8080/auth/google/callback",
+        // callbackURL: "http://localhost:8080/auth/google/callback",
+        callbackURL: process.env.NODE_ENV === "production"
+        ? "https://airbnb-clone-gbwk.onrender.com/auth/google/callback"
+        : "http://localhost:8080/auth/google/callback",
     } ,
     async (accessToken , refreshToken , profile, done ) => {
         try {
